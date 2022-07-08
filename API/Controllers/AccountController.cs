05f76011d6aa3015a4a9c67e8f67fafac6e5e7be
Fouche du Preez <fouche@limehouse.co.za>
2022-07-08 22:32:54 +0200

    Added initial Auth files

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,67 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+            // using ensures that variable gets disposed properly
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                Username = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            // only adds to EntityFramework context, not to DB
+            context.Users.Add(user);
+            // update the DB
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Username == loginDto.username);
+            if (user == null) return Unauthorized("Not authorized");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Not authorized");
+                }
+            }
+
+            return user;
+
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.Username == username.ToLower());
+        }
+    }
+}
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Password { get; set; }
+    }
+}
